ae6e49736e1e0331e2ecfc1140cd8525be81d817
shabshaeb <92678947+shabshaeb@users.noreply.github.com>
2021-11-07 15:05:37 +0700

    Update ModelBuku.php

--- app/Models/ModelBuku.php
+++ app/Models/ModelBuku.php
@@ -4,10 +4,10 @@ namespace App\Models;
 
 use Codeigniter\Model;
 
-class ModelPerpustakaan extends Model
+class ModelBuku extends Model
 {
     protected $table = "Buku";
     protected $primaryKey = "Kode_Buku";
-    protected $allowedFields = ['Judul_Buku','Kategori_Buku','Pengarang','Penerbit','Tahun_Terbit','Jumlah_Halaman','Jumlah_Eksemplar','Nomor_ISBN','Keterangan'];
+    protected $allowedFields = ['Judul_Buku','Kategori_Buku','Pengarang','Penerbit','Tahun_Terbit','Jumlah_Halaman','Jumlah_Eksemplar','Nomor_ISBN'];
 }
 
